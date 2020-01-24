23bd30f1dc93f37f731f35da4cfbb7ab34f03d64
Kevin Fallas <kevin.fallas.b@gmail.com>
2020-01-24 15:07:24 -0600

    crea gestor y modelo

---
+++ src/connect4server/model/ModeloConnect4.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package connect4server.model;
+
+import java.util.Observable;
+
+/**
+ *
+ * @author Kevin
+ */
+public class ModeloConnect4 extends Observable {
+    
+}
