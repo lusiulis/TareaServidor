d8fc767abe10b920f4989674c78cafdca463e8bf
Luis Vargas Benambourg <lusiulis@gmail.com>
2020-01-24 15:57:24 -0600

    Click

--- TareaServidor/src/tareaservidor/views/VentanaJuego.java
+++ TareaServidor/src/tareaservidor/views/VentanaJuego.java
@@ -3,6 +3,8 @@ package tareaservidor.views;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.Observable;
 import java.util.Observer;
 import javax.swing.JFrame;
@@ -14,7 +16,6 @@ public class VentanaJuego extends JFrame implements Observer{
     
     public VentanaJuego() {
         super("Juego");
-        casillas = new Casilla[8][8];
         configurar();
     }
 
@@ -33,16 +34,28 @@ public class VentanaJuego extends JFrame implements Observer{
 
     private void Ajustar(Container c) {
         c.setLayout(new GridLayout(8,8));
-        
+        casillas = new Casilla[8][8];
         for(int i=0;i<8;i++){
             for(int j=0;j<8;j++){
-                Casilla casilla = new Casilla(i,j);
+                Casilla casilla = new Casilla(j,i);
                 casilla.setBackground(Color.GREEN);
                 casilla.Actualizar();
+                AgregarEscucha(casilla);
                 casillas[i][j] = casilla;
                 c.add(casilla);
             }
         }
+    }
+    
+    private void AgregarEscucha(Casilla casilla){
+        casilla.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e){
+                if(!casilla.isClick()){
+                    System.out.println("x: "+casilla.getPosx()+", y: "+casilla.getPosy());
+                }
+            }
+        });
     }
     
     public void init(){
